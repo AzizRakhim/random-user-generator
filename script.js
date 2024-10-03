724bba15e31f8d0ea054a87a1f526b6af97d6241
Aziz Rakhim <azizrakhim17@gmail.com>
2024-10-03 14:50:35 +0500

    feat: added an error check on user fetch

--- script.js
+++ script.js
@@ -2,11 +2,22 @@ function fetchUser() {
   showSpinner();
 
   fetch("https://randomuser.me/api")
-    .then((res) => res.json())
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error("Request Failed");
+      }
+      return res.json();
+    })
     .then((data) => {
       hideSpinner();
 
       displayUser(data.results[0]);
+    })
+    .catch((error) => {
+      hideSpinner();
+      document.querySelector(
+        "#user"
+      ).innerHTML = `<p class="text-xl text-center text-red-500 mb-5">${error}</p>`;
     });
 }
 
